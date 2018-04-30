d3cf92f922c782f230cba866441dc76c51efb9b9
TUSD-Shane <spowell@students.tombstoneschools.org>
2018-04-30 05:40:51 -0700

    Added Exit Option

--- Program.cs
+++ Program.cs
@@ -8,10 +8,37 @@ namespace Nerd_Console
 {
     class Program
     {
+        private static bool canParse = new bool();
+        private static List<int> optionContainer = new List<int>();
+        private static int returnedOption = new int();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Please wait for further development...");
             Console.ReadLine();
+
+            bool canParse = false;
+            optionContainer.Add(1);
+            returnedOption = 0;
+
+            while (!canParse || !optionContainer.Contains(returnedOption))
+            {
+                Console.Clear();
+                Console.WriteLine("\n Please Enter The Index Of The Function You Wish To Run... ");
+                Console.WriteLine(" 1: Close Application. ");
+                Console.WriteLine("\n___________________________________________________________"); // Length of longest string!
+                canParse = int.TryParse(Console.ReadLine(), out int result);
+                returnedOption = result;
+            }
+            
+            switch (returnedOption)
+            {
+                case 1:
+                    {
+                        Environment.Exit(0);
+                        break;
+                    }
+            }
         }
     }
 }
