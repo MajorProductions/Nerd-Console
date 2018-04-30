fe444e2a2fa9ae6f1d48636eed1e052729a9239b
TUSD-Shane <spowell@students.tombstoneschools.org>
2018-04-30 16:52:25 -0700

    Reset Console
    Resetting so I can build everything up from the Nerd Lib.dll

--- Program.cs
+++ Program.cs
@@ -1,45 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NerdLib.Data;
+using NerdLib.Random;
 
-namespace Nerd_Console
+namespace NerdConsole
 {
     class Program
     {
-        private static bool canParse = new bool();
-        private static List<int> optionContainer = new List<int>();
-        private static int returnedOption = new int();
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Please wait for further development...");
-            Console.ReadKey();
-
-            bool canParse = false;
-            optionContainer.Add(1);
-            returnedOption = 0;
-
-            while (!canParse || !optionContainer.Contains(returnedOption))
-            {
-                Console.Clear();
-                Console.WriteLine("\n Please Enter The Index Of The Function You Wish To Run... ");
-                Console.WriteLine(" 1: Close Application. ");
-                Console.WriteLine("\n___________________________________________________________"); // Length of longest string!
-                Console.Write(" Index: ");
-                canParse = int.TryParse(Console.ReadLine(), out int result);
-                returnedOption = result;
-            }
-
-            switch (returnedOption)
-            {
-                case 1:
-                    {
-                        Environment.Exit(0);
-                        break;
-                    }
-            }
+            
         }
     }
 }
