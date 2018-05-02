80043800cab73ee0f6f1cb138299668f7efdb75f
MajorProductions <MajorProductions@Null.Net>
2018-05-01 22:39:30 -0700

    Major Update (Inc. Fixes to Random Gen Error)

--- Program.cs
+++ Program.cs
@@ -8,19 +8,74 @@ namespace NerdConsole
     {
         public static string inline = " >> ";
         public static string version = "V 1.0.0.0";
+        public static string logType = "";
+        public static string logLine = "";
 
         static void Main(string[] args)
         {
+            bool always = true;
+            while (always)
+            {
+                Console.Clear();
+                if (logLine != "") { if (logType == "debug") { Log.LogDebug(logLine); } if (logType == "warning") { Log.LogWarning(logLine); } if (logType == "error") { Log.LogError(logLine); } }
+                logLine = "";
+                int index = 0;
+                bool done = false;
+                Init();
+                Send.Message(" | Greetings User,                                |", false);
+                Send.Message(" | What Function Would You Like To Use?           |", false);
+                Send.Message(" | ---------------------------------------------- |", false);
+                Send.Message(" | 1: Random Number Generator.                    |", false);
+                Send.Message(" | 2: Exit Application.                           |", false);
+                Send.Message(" | ---------------------------------------------- |", false);
+                done = false;
+                while (!done)
+                {
+                    index = Input.RequestInt(" Choice >> ");
+                    if (index == 1) { RandomNumber(); done = true; }
+                    else if (index == 2) { Console.Quit(); done = true; }
+                    else { Console.Beep(); logLine = "That is not a valid index!"; logType = "error"; done = true; }
+                }
+            }
 
-            Console.Title("Nerd Console " + version);
+
+        }
+
+        static void Init()
+        {
+            True.SetKey("d627f22b-4a9d-4b3e-8883-05471e87e11f");
             Color.Background(Color.Colors.Black);
-            Color.Foreground(Color.Colors.Green);
+            Color.Foreground(Color.Colors.Gray);
+            Console.Title("Nerd Console");
+        }
 
-            Send.Message("This is a message", true);
-            Send.Message("This message is in blue", Color.Colors.Blue);
-
-            string tempfloat = Input.RequestBool("You saw that right?" + inline).ToString();
-            Send.Message("You said " + tempfloat + "!", true);
+        static void RandomNumber()
+        {
+            bool gen = true;
+            while (gen)
+            {
+                Console.Clear();
+                if (logLine != "") { if (logType == "debug") { Log.LogDebug(logLine); } if (logType == "warning") { Log.LogWarning(logLine); } if (logType == "error") { Log.LogError(logLine); } }
+                logLine = "";
+                int index = 0;
+                bool done = false;
+                Send.Message(" | Random Number                                  |", false);
+                Send.Message(" | ---------------------------------------------- |", false);
+                Send.Message(" | 1: Psuedo Random Generator.                    |", false);
+                Send.Message(" | 2: Unique Psuedo Random Generator.             |", false);
+                Send.Message(" | 3: True Random Generator.                      |", false);
+                Send.Message(" | 4: Back.                                       |", false);
+                Send.Message(" | ---------------------------------------------- |", false);
+                while (!done)
+                {
+                    index = Input.RequestInt(" Choice >> ");
+                    if (index == 1) { /*GetNumber*/ Console.Beep(); logLine = "Not Yet implemented"; logType = "warning"; done = true; }
+                    else if (index == 2) { /*GetNumber*/ Console.Beep(); logLine = "Not Yet implemented"; logType = "warning"; done = true; }
+                    else if (index == 3) { /*GetNumber*/ Console.Beep(); logLine = "Not Yet implemented"; logType = "warning"; done = true; }
+                    else if (index == 4) { gen = false; done = true; }
+                    else { Console.Beep(); logLine = "That is not a valid index!"; logType = "error"; done = true; }
+                }
+            }
         }
     }
 }
